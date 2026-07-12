f08022feb8d4fc215fdd59d0b954618730518173
Nizo <nizo@example.com>
2026-07-12 13:24:19 +0200

    Add Bootstrap JS bundle to enable dropdown menus

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,7 @@ import { router } from './router'
 import { i18n } from './i18n'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'leaflet/dist/leaflet.css'
 
 const pinia = createPinia()
